910eeeb64966b5837f2384e40ba978ed9bcdee1c
vvvc <mailtophj@foxmail.com>
2017-04-09 21:23:37 +0800

    make it loop 10 times

--- all_sample.py
+++ all_sample.py
@@ -14,7 +14,7 @@ def webster_sample(word):
 
     sample = soup.select("article div div div div ol li p em.vi")[0].get_text()
 
-    print(sample)
+    print('merriam-webster: ' + sample)
 
 def cambridge_sample(word):
     
@@ -28,7 +28,7 @@ def cambridge_sample(word):
 
     sample = soup.select("span div span.eg")[0].get_text()
 
-    print(sample)
+    print('cambridge: ' + sample)
 
 def iciba_sample(word):
 
@@ -42,17 +42,23 @@ def iciba_sample(word):
 
     sample = soup.select("div.sentence-item p.family-english span")[0].get_text()
 
-    print(sample)
+    print('iciba: ' + sample)
 
 
 def sample_collector(word):
 
-    print(word + ':\n')
+    print(word + ':')
     iciba_sample(word)
     webster_sample(word)
     cambridge_sample(word)
+    print('\n')
 
+for i in range(10):
 
-word = raw_input('input your word：')
+    word = raw_input('input your word：')
 
-sample_collector(word)
+    if word == 'exit':
+        break
+
+    sample_collector(word)
+
